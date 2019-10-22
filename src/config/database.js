4a3eb769936dc8b157e603f45f6eff794b23c29a
Albino Freitas <freitas@mageda.com.br>
2019-10-22 10:26:22 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '127.0.0.1',
+  username: 'root',
+  database: 'workshop-rest',
+  password: '123456',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
